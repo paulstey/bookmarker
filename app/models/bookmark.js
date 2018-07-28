4434b3442feac8adabac07e7a8bb84bff2ec6d78
paulstey <paulstey@gmail.com>
2018-07-28 19:58:51 -0400

    added several new routes and templates

---
+++ app/models/bookmark.js
@@ -0,0 +1,11 @@
+import DS from 'ember-data';
+import attr from 'ember-data/attr';
+
+export default DS.Model.extend({
+  link: attr(),
+  title: attr(),
+  about: attr(),
+  public: attr(),
+  created: attr()
+
+});
